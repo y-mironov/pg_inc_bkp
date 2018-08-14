829970065bb222888b5ca7b9e7a14368559d1e94
y-mironov <mironov.yy@gmail.com>
2018-08-14 15:05:46 +0300

    Update pg_bkp_inc.sh

--- pg_bkp_inc.sh
+++ pg_bkp_inc.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # CONFIG
-bkpdir=/mnt/backup/dbs            # backup root
-db=(mpru_lod_db mo_lod_db)        # databases for backup list
+bkpdir=/mnt/backup/dbs            # backup store
+dbs=(db_name_1 db_name_2)          # databases for backup list
 
 # MAIN
-date
 PATH=/bin:/usr/bin
 date=`date +%Y-%m-%d`
 mkdir -p ${bkpdir}/${date}/last
 dumpdir=${bkpdir}/${date}         # directory name for today dumps
 datehour=`date +%Y-%m-%d__%H`     # file name for current dumps
+db_host=127.0.0.1
 
-for (( i=0; i<${#db[*]}; i++ ))
+for (( i=0; i<${#dbs[*]}; i++ ))
 do
-    dbname=${db[$i]}
+    dbname=${dbs[$i]}
     fname=${dbname}__${datehour}.incbkp
 
     # check prev dump
     fresh_full_dump=$(ls -t ${dumpdir}/last/*${dbname}*.*bkp | head -1)
     echo "FRESH DUMP NAME: ${fresh_full_dump}"
     if [ -z ${fresh_full_dump} ]; then
-        echo "ERROR with dumping ${dbname}: not found last dump" > ${dumpdir}/${fname}.error
+        echo "ERROR with dumping ${dbname}: not found latest dump" > ${dumpdir}/${fname}.error
         continue
     fi
 
@@ -31,7 +31,7 @@ do
     rm ${dumpdir}/${fname}.error        # 2> /dev/null
 
     # dumping
-    PGPASSWORD=qbdhnLs4 nice pg_dump -U lod -Ox -T mamonsu_* -h 10.4.126.28 -d ${dbname} > ${dumpdir}/last/${fname}.part 2> ${dumpdir}/${fname}.error
+    PGPASSWORD=qbdhnLs4 nice pg_dump -U lod -Ox -T mamonsu_* -h ${db_host} -d ${dbname} > ${dumpdir}/last/${fname}.part 2> ${dumpdir}/${fname}.error
     if [ -s ${dumpdir}/${fname}.error ]; then
         rm ${dumpdir}/last/${fname}.part
         echo "ERROR with dumping ${dbname}: "
